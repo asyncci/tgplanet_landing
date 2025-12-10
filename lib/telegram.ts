83a96c3e48777c2b0f4542a0a5bc50fc0c89df07
adilet <asyncci@github>
2025-12-10 13:37:44 +0600

    .env fix

--- lib/telegram.ts
+++ lib/telegram.ts
@@ -1,8 +1,8 @@
 // lib/telegram.ts
 
 // Получи эти данные у @BotFather и в чате с ботом
-const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN!;
-const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID!; // твой личный чат ID или ID группы
+const TELEGRAM_BOT_TOKEN = process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN!;
+const TELEGRAM_CHAT_ID = process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID!; // твой личный чат ID или ID группы
 
 export async function sendTelegramMessage(message: string) {
   const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
